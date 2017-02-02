7d96ccb00120d140bb9df75709e6151262a36951
Cecilia Ulrich <cecilia.ulrich@logicline.de>
2017-02-02 09:52:43 +0100

    Node.js express cluster module

---
+++ config/config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  // used when we create and verify JSON Web Tokens
+  'secret': 'protectedBaderBoardByLL'
+};
---
+++ server.js
@@ -0,0 +1,55 @@
+var cluster = require('cluster');
+
+if (cluster.isMaster)
+{
+  console.log('Server is active. Forking workers now.');
+  var cpuCount = require('os').cpus().length;
+  for (var i=0; i<cpuCount; i++)
+  {
+    cluster.fork();
+  }
+  cluster.on('exit', function(worker)
+  {
+    console.error('Worker %s has died! Creating a new one.', worker.id);
+    cluster.fork();
+  });
+}
+else {
+  const cookieSession = require('cookie-session');
+  const express = require('express');
+  const app = express();
+  const bodyParser = require('body-parser');
+  const port = process.env.PORT || 8090;
+  const methodOverride = require('method-override');
+  const api = require('./routes/api');
+  const config = require('./config/config');
+
+  // Handle body
+  app.use(bodyParser.json());
+
+// Parse application/json as json
+  app.use(bodyParser.json({type: 'application/json; charset=utf-8'}));
+
+// Override with the X-HTTP-Method-Override header in the request
+  app.use(methodOverride('X-HTTP-Method-Override'));
+
+// Session
+  app.use(cookieSession({
+    name: 'session',
+    keys: [config.secret],
+
+    // Cookie Options
+    maxAge: 24 * 60 * 60 * 1000 // 24 hours
+  }));
+
+  app.get("/", api.getalluser);
+  app.get('/user/:id', api.getuser);
+  app.post('/user', api.createuser);
+  app.put('/user/:id', api.changeuser);
+  app.delete('/user/:id', api.deletuser);
+
+  app.listen(port, function () {
+    console.log('Worker %s spawned for port %s.', cluster.worker.id, port);
+  });
+
+}
